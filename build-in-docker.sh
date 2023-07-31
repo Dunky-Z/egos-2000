b989bacfa547a4e5c900f7dba2bebb5ac9e39366
Dongdong Zhang <zhangdongdong@eswincomputing.com>
2023-07-31 21:06:12 +0800

    update

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,13 +1,11 @@
 #!/bin/zsh
 
-
-# By default run with docker, if podman is available use that
 DOCKER_CMD=docker
 EGOS_2000_CONTAINER_TAG=latest
-# The name of the container image we run
 EGOS_2000_CONTAINER=docker.io/tclab/ucore-env:$EGOS_2000_CONTAINER_TAG
 
 RUN=${@:-bash}
+
 # Run the given command in the container
 # --rm to clean up the container afterwards. It doesn't contain any state.
 #      Everything is in the local directory, since we're fully mounting it into
@@ -22,5 +20,8 @@ $DOCKER_CMD run \
   -e PATH=$PATH:/riscv64-acpi/dl/toolchain/bin \
   -e WORK_DIR=/home/user/egos-2000-dev \
   -e GCC5_RISCV64_PREFIX=riscv64-unknown-elf- \
+  -e EGOS=/home/user/egos-2000-dev \
   -it "$EGOS_2000_CONTAINER" \
-    $RUN
+   /bin/bash -c "echo 'user ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers &&  su user && zsh"
+#    /bin/bash -c "useradd -d /home/user user -M -s /bin/bash && echo 'user ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers && su user && zsh"
+
